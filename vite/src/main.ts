46e7309b06d9504c3c45a138a63efb705b816a3a
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2022-12-20 09:24:00 +0100

    MDölja och visa varukorgen

--- vite/src/main.ts
+++ vite/src/main.ts
@@ -1,5 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import './style.css'
 
+// Open Cart
+document.querySelector('.cart-icon')!.addEventListener('click', () => {
+    document.querySelector('.cart-container')!.classList.remove('d-none')
+})
 
-
+// Close cart
+document.querySelector('.cart-close')!.addEventListener('click', () => {
+    document.querySelector('.cart-container')!.classList.add('d-none')
+})
